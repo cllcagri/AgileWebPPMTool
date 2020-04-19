1bec462296de1770925468552d6f273d39fab9bf
celal <c.cagriyilmaz@gmail.com>
2020-04-19 22:14:43 +0300

    Created User Model  and JWT Generated

--- PPMToolFullStack/src/main/java/io/growbymastery/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
+++ PPMToolFullStack/src/main/java/io/growbymastery/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
@@ -23,4 +23,10 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
         ProjectNotFoundException exceptionResponse = new ProjectNotFoundException(ex.getMessage());
         return new ResponseEntity<Object>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleUsernameAlreadyExistException(UsernameAlreadyExistException ex, WebRequest request){
+        UsernameAlreadyExistException exceptionResponse = new UsernameAlreadyExistException(ex.getMessage());
+        return new ResponseEntity<Object>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
 }
